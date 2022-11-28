c5b62f4f0f1465ca8204204d85a51b5de05c0dd2
daiyu <daiyu@wsecar.com>
2022-11-28 17:04:34 +0800

    perf: 🎈 更新vant样式变量

--- src/assets/style/index.ts
+++ src/assets/style/index.ts
@@ -2,7 +2,7 @@
  * @Author: DaiYu
  * @Date: 2022-10-18 19:57:21
  * @LastEditors: DaiYu
- * @LastEditTime: 2022-10-27 10:46:44
+ * @LastEditTime: 2022-11-28 17:02:08
  * @FilePath: \src\assets\style\index.ts
  */
 import 'normalize.css'
@@ -10,5 +10,6 @@ import 'vant/es/toast/style'
 import 'vant/es/dialog/style'
 import 'animate.css'
 import './base.css'
+import './vant.css'
 import 'uno.css'
 // import 'virtual:unocss-devtools' // 调试模式
